34c758293d144686f87ee6ff86dda764a8706a6e
andresmiranda <71046677+andrymei@users.noreply.github.com>
2020-09-13 18:04:29 -0300

    Update apl1-ejercicio4.sh
    Habria que buscarle la forma para que libere la terminal, y que no consuma tanto recurso.

--- apl1-ejercicio4.sh
+++ apl1-ejercicio4.sh
@@ -93,16 +93,21 @@ echo ""
         ((i++))
     done
     
-    for i in "${!blackproc[@]}" 
-    do 
-        proceso_encontrado=$(ps aux|grep ${blackproc[i]}| grep -v "grep" -c)
-        if [ $proceso_encontrado -gt 0 ]
-        then
-            fecha="$(date +"%Y-%m-%d_%H:%M:%S")"
-            killed_pids="$(ps aux | grep ${blackproc[i]} | grep -v "grep" | awk '{print $1,$2,$11}')"
-            echo $fecha $killed_pids >> "$directorioSalida/logfile.log" 
+    while true;
+    do
+        for i in "${!blackproc[@]}" 
+        do 
+            proceso_encontrado=$(ps aux|grep ${blackproc[i]}| grep -v "grep" -c)
+            if [ $proceso_encontrado -gt 0 ]
+            then
+                fecha="$(date +"%Y-%m-%d_%H:%M:%S")"
+                killed_pids="$(ps aux | grep ${blackproc[i]} | grep -v "grep" | awk '{print $1,$2,$11}')"
+                echo $fecha $killed_pids >> "$directorioSalida/logfile.log" 
 
-            killall $killed_pids 2> /dev/null #para que no se vea en la terminal la salida de la pantalla.
+                killall $killed_pids 2> /dev/null #para que no se vea en la terminal la salida de la pantalla.
 
-        fi    
+            fi    
+        done
     done
+    
+#FIN   
