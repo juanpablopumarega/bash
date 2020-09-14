624dec6805f77bd75be227e034eb96ae0a91f3f8
andresmiranda <71046677+andrymei@users.noreply.github.com>
2020-09-13 21:00:46 -0300

    Update APL1ejercicio4.sh

--- APL1ejercicio4.sh
+++ APL1ejercicio4.sh
@@ -86,37 +86,9 @@ callSintaxError() {
     export directorioSalida
     export outputFileName
     export archivoDeBlacklist
-    
+# Se le da permisos de ejecucion al segundo script, y se lo ejecuta en segundo plano    
     chmod +x "$PWD/blacklist.sh"
     ./blacklist.sh &
 
-##Aca empieza el algoritmo matador de procesos...
-#    declare -a blackproc
-#    i=0;
-#    for proceso in $(cat "$archivoDeBlacklist" ) #| tr ‘[A-Z]’ ‘[a-z]’)
-#    do
-#        blackproc[i]="$proceso"
-#        ((i++))
-#    done
-#
-#    #for i in "${!blackproc[@]}" 
-#    #do 
-#    #    echo "${blackproc[i]}"
-#    #done
-#    while true;
-#    do    
-#        for i in "${!blackproc[@]}" 
-#        do 
-#            proceso_encontrado=$(ps aux|grep ${blackproc[i]}| grep -v "grep" -c)
-#            if [ $proceso_encontrado -gt 0 ]
-#            then
-#                fecha="$(date +"%Y-%m-%d_%H:%M:%S")"
-#                killed_pids="$(ps aux | grep ${blackproc[i]} | grep -v "grep" | awk '{print $1,$2,$11}')"
-#                echo $fecha $killed_pids >> "$directorioSalida/$outputFileName" 
-#
-#                killall $killed_pids 2> /dev/null #para que no se vea en la terminal la salida de la pantalla.
-#
-#            fi    
-#        done
-#    done
+
 #FIN
