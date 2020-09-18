eac5fc80f51b3337f091f9d6627fc884fddb854a
andresmiranda <71046677+andrymei@users.noreply.github.com>
2020-09-18 00:28:31 -0300

    Ejercicio 4 cambio de nombre para script

---
+++ APL1Ejercicio4/blacklistKiller.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+declare -a blackproc
+    i=0;
+    for proceso in $(cat "$archivoDeBlacklist" )
+    do
+        blackproc[i]="$proceso"
+        ((i++))
+    done
+
+    while true;
+    do    
+        for i in "${!blackproc[@]}" 
+        do 
+            proceso_encontrado=$(ps aux|grep ${blackproc[i]}| grep -v "grep" -c)
+            if [ $proceso_encontrado -gt 0 ]
+            then
+                fecha="$(date +"%Y-%m-%d_%H:%M:%S")"
+                killed_pids="$(ps aux | grep ${blackproc[i]} | grep -v "grep" | awk '{print $1,$2,$11}')"
+                echo $fecha $killed_pids >> "$directorioSalida/$outputFileName" 
+                #para que no se vea en la terminal la salida de la pantalla.
+                killall $killed_pids 2> /dev/null 
+
+            fi    
+        done
+    done
